a2ec10fb0cdbb6d9fae91ae490e811cd76ed4bca
truongvanviet <truong_van_viet@yahoo.com.vn>
2014-04-06 21:48:05 +0700

    file_themefunction

---
+++ title-image.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<h1><?php print $title;?></h1>
+<?php print theme('image', array('path' => $image));?>
+<h1><?php print $description;?></h1>
